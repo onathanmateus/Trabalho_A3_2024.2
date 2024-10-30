c6c4dc7d18d7c0427616922a9f559909c7fa5fe7
Arthur Wagner <lushtergamernoob@gmail.com>
2024-10-30 20:49:08 -0300

    Adicionei o fundo verde

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div className="m-0 flex h-screen w-screen items-center justify-center p-0">
+    <div className="m-0 flex h-screen w-screen items-center justify-center p-0"
+    style={{ backgroundColor: '#EAF9E4' }} >
       HELLO
     </div>
   )
