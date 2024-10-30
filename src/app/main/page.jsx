ce755287ed0877e9b0397d56e50304278850304e
Nathan Mateus <nathanmateudeo@hotmail.com>
2024-10-30 19:47:56 -0300

    Modifiquei o nome da função da página principal

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 
-export default function Login() {
+export default function Main() {
   return (
     <div className="m-0 flex h-screen w-screen items-center justify-center p-0">
       HELLO
