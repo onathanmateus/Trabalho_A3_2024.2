1f78df1cb7de894d996bcac93f82f6ca05687eea
Nathan Mateus <nathanmateudeo@hotmail.com>
2024-10-31 07:47:32 -0300

    Alterei o nome da rota da página principal para 'Home'

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 
-export default function Main() {
+export default function Home() {
   return (
     <div className="m-0 flex h-screen w-screen items-center justify-center bg-[#EAF9E4] p-0">
       HELLO
