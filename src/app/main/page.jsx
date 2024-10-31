d9508b7ee5a9b70bb1a801e919419ec598421f4e
Nathan Mateus <nathanmateudeo@hotmail.com>
2024-10-31 07:46:14 -0300

    Corrigi o mode de estilizar e converti para tailwindcss

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div className="m-0 flex h-screen w-screen items-center justify-center p-0"
-    style={{ backgroundColor: '#EAF9E4' }} >
+    <div className="m-0 flex h-screen w-screen items-center justify-center bg-[#EAF9E4] p-0">
       HELLO
     </div>
   )
