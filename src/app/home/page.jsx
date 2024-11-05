4ce478d2a5d6821625410a95fc7a92b89ad67bfc
PedroVitor10 <pedrovitorpvcnrn@gmail.com>
2024-11-04 22:29:30 -0300

    Arrumei o tamanho dos cards para um padrão x, adicionei novos produtos, coloquei seus nomes bem como uma breve descrição

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -5,22 +5,46 @@ import Image from 'next/image';
 import feeds2 from '../../../public/feeds2.png';
 import iphone from '../../../public/Iphone.png';
 import samsung from '../../../public/Samsung.png';
+import jbl from '../../../public/fone.png';
+import notebook from '../../../public/notebook.png';
+import camisa from '../../..//public/camisa.png'; 
 
 // Array de produtos
 const produtos = [
   {
     id: 1,
-    nome: 'iPhone',
+    nome: 'Apple iPhone 14 (128 GB) – Roxo',
     imagem: iphone,
-    preco: 'R$99,90',
-    info: ['Informação Adicional 1', 'Informação Adicional 2', 'Informação Adicional 3']
+    preco: 'R$4199 ou 12x de R$ 388,80 sem juros',
+    info: ['Tela Super Retina XDR 6,1 polegadas', 'Modo Cinema agora em 4K Dolby Vision até 30 qps', '']
   },
   {
     id: 2,
-    nome: 'Samsung',
+    nome: 'Celular Samsung Galaxy A55 5G',
     imagem: samsung,
-    preco: 'R$99,90',
-    info: ['Informação Adicional 1', 'Informação Adicional 2', 'Informação Adicional 3']
+    preco: 'R$429,00 a vista ou 12x de R$ 35,75',
+    info: ['Câmera tripla 50MP', 'Tela Super AMOLED FHD+ de 6.6"', '256 GB Azul Claro']
+  },
+  {
+    id: 3,
+    nome: 'Fone de ouvido JBL',
+    imagem: jbl,
+    preco: 'R$298,99 ou 12x de R$ 28,99',
+    info: ['Sem fio', 'Possui microfone', 'Bateria: 76 horas']
+  },
+  {
+    id: 4,
+    nome: 'Notebook Ultra com Windows 11 Home',
+    imagem: notebook,
+    preco: 'R$1398,00 ou até 10x R$ 139,80 sem juros',
+    info: ['Processador Celeron N4020C', '128GB', '4GB RAM']
+  },
+  {
+    id: 5,
+    nome: 'Camisa do Flamengo 24/25 Masculina',
+    imagem: camisa, 
+    preco: 'R$349,99 ou 5x de 69,99',
+    info: ['Modelo Torcedor', 'Marca Adidas', '100% Poliéster']
   }
 ];
 
@@ -34,9 +58,9 @@ export default function ProductPage() {
       </div>
       
       {/* Informação dos produtos */}
-      <div className="flex grow items-start justify-start w-full px-4 mt-2 space-x-8">
+      <div className="flex grow flex-wrap items-start justify-start w-full px-4 mt-2 space-x-8">
         {produtos.map(produto => (
-          <div key={produto.id} className="flex flex-col items-center bg-white p-4 shadow-md rounded-lg">
+          <div key={produto.id} className="flex flex-col items-center bg-white p-4 shadow-md rounded-lg" style={{ width: '250px', height: '400px' }}>
             <div className="w-[200px] h-[200px] relative">
               <Image src={produto.imagem} alt={`Product Image of ${produto.nome}`} layout="fill" objectFit="contain" />
             </div>
@@ -50,7 +74,6 @@ export default function ProductPage() {
           </div>
         ))}
       </div>
-      
     </div>
   );
 }
